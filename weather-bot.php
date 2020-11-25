4f732d5a7f4635a94c6db3701263b6e52dc826f2
Krutik <58563133+reboot13-git@users.noreply.github.com>
2020-11-25 15:26:41 +0530

    Create weather-bot.php

---
+++ weather-bot.php
@@ -0,0 +1,80 @@
+<?php
+    date_default_timezone_set("Asia/kolkata");
+    //Data From Webhook
+    $content = file_get_contents("php://input");
+    $update = json_decode($content, true);
+    $chat_id = $update["message"]["chat"]["id"];
+    $message = $update["message"]["text"];
+    $id = $update["message"]["from"]["id"];
+    $username = $update["message"]["from"]["username"];
+    $firstname = $update["message"]["from"]["first_name"];
+    $message_id = $upadte["message"]["message_id"];
+
+    //Start message
+    if($message == "/start"){
+        send_message($chat_id, "Hey $firstname  \nUse /weather <word>   \nWeather Bot by @reboot13 ");
+    }
+
+
+
+if(strpos($message, "/weather") === 0){
+        $location = substr($message, 9);
+   $curl = curl_init();
+   curl_setopt_array($curl, [
+CURLOPT_URL => "http://api.openweathermap.org/data/2.5/weather?q=$location&appid=89ef8a05b6c964f4cab9e2f97f696c81",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 50,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => [
+    "Accept: */*",
+        "Accept-Language: en-GB,en-US;q=0.9,en;q=0.8,hi;q=0.7",
+        "Host: api.openweathermap.org",
+        "sec-fetch-dest: empty",
+    "sec-fetch-site: same-site"
+  ],
+]);
+
+
+$content = curl_exec($curl);
+curl_close($curl);
+$resp = json_decode($content, true);
+
+$weather = $resp['weather'][0]['main'];
+$description = $resp['weather'][0]['description'];
+$temp = $resp['main']['temp'];
+$humidity = $resp['main']['humidity'];
+$feels_like = $resp['main']['feels_like'];
+$country = $resp['sys']['country'];
+$name = $resp['name'];
+$kelvin = 273;
+$celcius = $temp - $kelvin;
+$feels = $feels_like - $kelvin;
+
+if ($location = $name) {
+        send_message($chat_id, "
+    Weather at $location: $weather
+Status: $description
+Temp : $celcius °C
+Feels Like : $feels °C
+Humidity: $humidity
+Country: $country 
+Checked By @$username ");
+}
+else {
+           send_message($chat_id, "Invalid City");
+}
+    }
+    
+    
+//Send Messages (Global)
+      function send_message($chat_id, $message){
+       $apiToken = "Your Bot API Token";
+        $text = urlencode($message);
+        file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=$text");
+    }
+    
+?>
