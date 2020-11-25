16152719a9c23f3315a9d6ec29ee6099876c7899
Krutik <58563133+reboot13-git@users.noreply.github.com>
2020-11-25 14:43:01 +0530

    Add files via upload

---
+++ bin-checker-bot.php
@@ -0,0 +1,84 @@
+<?php
+    date_default_timezone_set("Asia/kolkata");
+    //Data From Webhook
+    $content = file_get_contents("php://input");
+    $update = json_decode($content, true);
+    $chat_id = $update["message"]["chat"]["id"];
+    $message = $update["message"]["text"];
+    $id = $update["message"]["from"]["id"];
+    $username = $update["message"]["from"]["username"];
+    $firstname = $update["message"]["from"]["first_name"];
+    $message_id = $upadte["message"]["message_id"];
+
+    //Start message
+    if($message == "/start"){
+        send_message($chat_id, "Hey $firstname  \nUse /bin xxxxxx To check Bins  \nBot by @reboot13 ");
+    }
+
+
+
+//Bin Lookup
+   if(strpos($message, "/bin") === 0){
+        $bin = substr($message, 5);
+   $curl = curl_init();
+   curl_setopt_array($curl, [
+	CURLOPT_URL => "https://lookup.binlist.net/".$bin,
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_ENCODING => "",
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => "GET",
+	CURLOPT_HTTPHEADER => [
+		"authority: lookup.binlist.net",
+		"accept: application/json",
+		"accept-language: en-GB,en-US;q=0.9,en;q=0.8,hi;q=0.7",
+		"origin: https://binlist.net",
+		"https://binlist.net/",
+		"sec-fetch-dest: empty",
+		"sec-fetch-site: same-site"
+	],
+]);
+
+$result = curl_exec($curl);
+curl_close($curl);
+$data = json_decode($result, true);
+$bank = $data['bank']['name'];
+$country = $data['country']['alpha2'];
+$currency = $data['country']['currency'];
+$emoji = $data['country']['emoji'];
+$scheme = $data['scheme'];
+$Brand = $data['brand'];
+$type = $data['type'];
+  if ($scheme != null) {
+        send_MDmessage($chat_id, " **
+    Bin: $bin
+Type: $scheme
+Brand : $Brand
+Bank: $bank
+Country: $country $emoji
+Currency: $currency
+Credit/Debit:$type
+Checked By @$username ** ");
+    }
+else {
+    send_MDmessage($chat_id, "Enter Valid BIN");
+}
+   }
+    
+
+
+
+
+
+
+    
+//Send Messages with Markdown (Global)
+      function send_MDmessage($chat_id, $message){
+       $apiToken = "Your Bot API Token";
+        $text = urlencode($message);
+        file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=$message&parse_mode=Markdown");
+    }
+    
+?>
